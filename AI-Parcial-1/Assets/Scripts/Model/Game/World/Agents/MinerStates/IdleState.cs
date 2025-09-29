b2ad98bc37f641ca6ae6c6a01f3f14728053f36d
TopoFabbri <66918573+TopoFabbri@users.noreply.github.com>
2025-09-29 19:58:12 -0300

    Revert "Added recall food request"
    This reverts commit df17f05f6023905cba42da83a483aadf8359c38f.
    
    # Conflicts:
    #	AI-Parcial-1/Assets/Scripts/Model/Game/World/Agents/MinerStates/IdleState.cs

--- AI-Parcial-1/Assets/Scripts/Model/Game/World/Agents/MinerStates/IdleState.cs
+++ AI-Parcial-1/Assets/Scripts/Model/Game/World/Agents/MinerStates/IdleState.cs
@@ -10,12 +10,7 @@ namespace Model.Game.World.Agents.MinerStates
 {
     public class IdleState : State
     {
-        private Timer timer;
         private FoodContainer foodContainer;
-        private Coordinate currentCoordinate;
-
-        private const float TimeToRequestFood = 5f;
-
         public override Type[] OnEnterParamTypes => new[]
         {
             typeof(Graph<Node<Coordinate>, Coordinate>),
@@ -30,7 +25,6 @@ namespace Model.Game.World.Agents.MinerStates
             BehaviourActions behaviourActions = Pool.Get<BehaviourActions>();
 
             behaviourActions.AddMultiThreadableBehaviour(0, () => { SetFoodContainer(graph, coordinate); });
-            behaviourActions.AddMultiThreadableBehaviour(0, StartTimer);
 
             return behaviourActions;
         }
@@ -40,14 +34,12 @@ namespace Model.Game.World.Agents.MinerStates
             BehaviourActions behaviourActions = Pool.Get<BehaviourActions>();
 
             behaviourActions.AddMultiThreadableBehaviour(0, CheckIfEnded);
-            behaviourActions.AddMultiThreadableBehaviour(0, CheckTimer);
 
             return behaviourActions;
         }
 
         private void SetFoodContainer(Graph<Node<Coordinate>, Coordinate> graph, Coordinate coordinate)
         {
-            currentCoordinate = coordinate;
             if (graph == null) return;
 
             foreach (INodeContainable<Coordinate> nodeContainable in graph.GetNodeAt(coordinate).GetNodeContainables())
@@ -61,19 +53,6 @@ namespace Model.Game.World.Agents.MinerStates
         {
             if (foodContainer == null || !foodContainer.IsEmpty)
                 flag.Invoke(Miner.Flags.IdleEnded);
-        }
-
-        private void CheckTimer()
-        {
-            if (timer == null || timer.TimeElapsed > TimeToRequestFood) return;
-            
-            FoodRequestSystem.RequestFood(currentCoordinate);
-            timer = new Timer();
-        }
-
-        private void StartTimer()
-        {
-            timer = new Timer();
         }
     }
 }
